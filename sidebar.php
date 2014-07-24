183f2dc4947c265a0e227e80c41d6776a74ac9b6
literat <tomaslitera@hotmail.com>
2014-07-24 11:45:13 +0200

    Use bootstrap nav walker in a sidebar (refs #1)

--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,19 @@
 <div id="secondary">
 	<?php if ( has_nav_menu( 'secondary' ) ) : ?>
 	<nav role="navigation" class="navigation site-navigation secondary-navigation">
-		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
+		<?php
+			wp_nav_menu( array(
+				'menu'              => 'secondary',
+				'theme_location'    => 'secondary',
+				'depth'             => 3,
+				'container'         => 'div',
+				'container_class'   => 'collapse navbar-collapse',
+				'container_id'      => 'bs-example-navbar-collapse-1',
+				'menu_class'        => 'nav navbar-nav sidebar-nav',
+				'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+				'walker'            => new wp_bootstrap_navwalker())
+			);
+		?>
 	</nav>
 	<?php endif; ?>
 
