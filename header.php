868758ecc0b08ab43e987c2faac59c867ed716aa
literat <tomaslitera@hotmail.com>
2015-01-16 14:01:30 +0100

    Add support for html5 tags for ie 9 and lower (refs #16)

--- header.php
+++ header.php
@@ -21,10 +21,8 @@
 		?>
 		<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/<?php echo $dsw_style_sheet?>.css" type="text/css" media="screen" />
 
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 		<![endif]-->
 
 		<?php wp_head(); ?>
