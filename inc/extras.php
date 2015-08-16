905de99e82889afef3e7a0f7807673af5ba49852
literat <tomas@litera.me>
2015-08-16 20:53:56 +0200

    fix: Resolving TODO, because WP version is greater then 4.3 (refs #16)

--- inc/extras.php
+++ inc/extras.php
@@ -82,18 +82,6 @@ if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
 	}
 	add_filter( 'wp_title', 'dswoddil_wp_title', 10, 2 );
 
-	/**
-	 * Title shim for sites older than WordPress 4.1.
-	 *
-	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function dswoddil_render_title() {
-		?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
-		<?php
-	}
-	add_action( 'wp_head', 'dswoddil_render_title' );
 endif;
 
 /******************************************************************************
