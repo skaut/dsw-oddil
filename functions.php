b914e7f7266c3d763ddeb80a96e9a0ac78e23969
literat <tomas@litera.me>
2015-08-18 10:30:13 +0200

    fix: Fixing additional widget area that appears in the admin (refs #16)

--- functions.php
+++ functions.php
@@ -239,16 +239,6 @@ function dswoddil_widgets_init() {
 }
 add_action( 'widgets_init', 'dswoddil_widgets_init' );
 
-if ( function_exists('register_sidebar') )
-	register_sidebar(array(
-		'id'			=> 'widget',
-		'before_widget'	=> '',
-		'after_widget'	=> '',
-		'before_title'	=> '<h3>',
-		'after_title'	=> '</h3>',
-	)
-);
-
 // Implement Custom Header features.
 require get_template_directory() . '/inc/custom-header.php';
 
