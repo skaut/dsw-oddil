30d4581d82cfe583a3bb308244a5f68ae0ce354c
literat <tomas@litera.me>
2015-09-23 14:00:40 +0200

    Clean up theme options (closes #52)

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -128,101 +128,6 @@ function dswoddil_theme_options_help() {
 	}
 }
 
-
-
-
-/*
-add_action( 'admin_menu', 'dswoddil_theme_settings_menu' );
-add_action( 'admin_init', 'dswoddil_theme_settings_init');
-*/
-
-/**
- * Preparing theme settings into menu.
- *
- * @since DSW oddil 1.0
- */
-/*
-function dswoddil_theme_settings_menu() {
-	add_theme_page(
-		__( 'Theme Settings', 'dswoddil' ),
-		__( 'Theme Settings', 'dswoddil' ),
-		'administrator',
-		'dswoddil_theme_settings',
-		'dswoddil_theme_settings_page_render'
-	);
-}
-*/
-
-/**
- * Render theme settings page.
- *
- * @since DSW oddil 1.0
- */
-/*
-function dswoddil_theme_settings_page_render() {
-	// Create a header in the default WordPress 'wrap' container
-	?>
-	<div class="wrap">
-		<h2><?php _e( 'DSW Oddil Theme Settings', 'dswoddil' )?></h2>
-		<?php settings_errors(); ?>
-
-		<form method="post" action="options.php">
-			<?php settings_fields( 'dswoddil_theme_settings_page' ); ?>
-			<?php do_settings_sections( 'dswoddil_theme_settings_page' ); ?>
-			<?php submit_button(); ?>
-		</form>
-
-	</div>
-	<?php
-}
-*/
-
-/**
- * Initialization of theme settings.
- *
- * @since DSW oddil 1.0
- */
-/*
-function dswoddil_theme_settings_init() {
-	if ( false == get_option( 'dswoddil_theme_settings_page' ) ) {
-		add_option( 'dswoddil_theme_settings_page' );
-	}
-
-	add_settings_section(
-		'dswoddil_general_settings_section',
-		__( 'Layout Settings', 'dswoddil' ),
-		'dswoddil_layout_settings_callback',
-		'dswoddil_theme_settings_page'
-	);
-
-	add_settings_field(
-		'dswoddil_layout_color',
-		__( 'Layout color', 'dswoddil' ),
-		'dswoddil_layout_color_switcher_render',
-		'dswoddil_theme_settings_page',
-		'dswoddil_general_settings_section',
-		array(
-			__( 'Change this setting to display different color.', 'dswoddil' )
-		)
-	);
-
-	register_setting(
-		'dswoddil_theme_settings_page',
-		'dswoddil_layout_color'
-	);
-}
-*/
-/**
- * Layout settings callback.
- *
- * @since DSW oddil 1.0
- */
-/*
-function dswoddil_layout_settings_callback() {
-	_e( '<p>Select which layout color you wish to display.</p>', 'dswoddil' );
-}
-*/
-
 /**
  * Returns the default options for DSW Oddil.
  *
@@ -357,39 +262,6 @@ function dswoddil_theme_options_render_page() {
 }
 
 /**
- * Render layout color switcher.
- *
- * @since DSW oddil 1.0
- */
-/*
-function dswoddil_layout_color_switcher_render($args) {
-	$options = array (
-		"id"   	 => "dswoddil_layout_color",
-		"colors" => array(
-			"red" 		=> __( 'red', 'dswoddil' ),
-			"blue"		=> __( 'blue', 'dswoddil' ),
-			"violet"	=> __( 'violet', 'dswoddil' ),
-			"green"		=> __( 'green', 'dswoddil' )
-		),
-	);
-
-	$html = '<select style="width:200px;" name="'.$options['id'].'" id="'.$options['id'].'">';
-				foreach ( $options['colors'] as $color_key => $color_value ) {
-					$html .= '<option';
-					if (get_option( $options['id'] ) == $color_key) {
-						$html .= ' selected="selected"';
-					}
-					$html .= ' value = "'.$color_key.'">'.$color_value.'</option>';
-				}
-				$html .= '</select>';
-
-	// Here, we will take the first argument of the array and add it to a label next to the checkbox
-	$html .= '<label for="dswoddil_layout_color"> ' . $args[0] . '</label>';
-
-	echo $html;
-}
-*/
-/**
  * Sanitize and validate form input. Accepts an array, return a sanitized array.
  *
  * @see dswoddil_theme_options_init()
@@ -425,17 +297,3 @@ function dswoddil_enqueue_color_scheme() {
 	do_action( 'dswoddil_enqueue_color_scheme', $color_scheme );
 }
 add_action( 'wp_enqueue_scripts', 'dswoddil_enqueue_color_scheme' );
-
-/******************************************************************************
-	ADMIN SCRIPTS
-******************************************************************************/
-/*
-function dswoddil_load_custom_wp_admin_scripts()
-{
-	// Register the script like this for a theme:
-	//wp_register_script( 'custom-script', get_template_directory_uri() . '/js/' . ((dswoddil_get_dev_enviroment() <> 1) ? 'bootstrap.js' : 'bootstrap.min.js') , array( 'jquery' ) );
-	// For either a plugin or a theme, you can then enqueue the script:
-	//wp_enqueue_script( 'custom-script' );
-}
-add_action( 'admin_enqueue_scripts', 'dswoddil_load_custom_wp_admin_scripts' );
-*/
