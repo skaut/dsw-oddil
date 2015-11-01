b2ef356dcb4ff935d581506b3f4027966dae19d7
literat <tomas@litera.me>
2015-11-01 13:47:28 +0100

    Correct indenting

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -47,98 +47,98 @@ function dswoddil_custom_header_setup() {
 }
 add_action( 'after_setup_theme', 'dswoddil_custom_header_setup' );
 
-if ( ! function_exists( 'dswoddil_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog
- *
- * @see dswoddil_custom_header_setup().
- *
- */
-function dswoddil_header_style() {
-	$text_color = get_header_textcolor();
+if ( ! function_exists( 'dswoddil_header_style' ) ) {
+	/**
+	 * Styles the header image and text displayed on the blog
+	 *
+	 * @see dswoddil_custom_header_setup().
+	 *
+	 */
+	function dswoddil_header_style() {
+		$text_color = get_header_textcolor();
 
-	// If no custom color for text is set, let's bail.
-	if ( display_header_text() && $text_color === get_theme_support( 'custom-header', 'default-text-color' ) )
-		return;
+		// If no custom color for text is set, let's bail.
+		if ( display_header_text() && $text_color === get_theme_support( 'custom-header', 'default-text-color' ) )
+			return;
 
-	// If we get this far, we have custom styles.
-	?>
-	<style type="text/css" id="dswoddil-header-css">
-	<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-	?>
-		.site-title,
-		.site-description {
-			clip: rect(1px 1px 1px 1px); /* IE7 */
-			clip: rect(1px, 1px, 1px, 1px);
-			position: absolute;
-		}
-	<?php
-		// If the user has set a custom color for the text, use that.
-		elseif ( $text_color != get_theme_support( 'custom-header', 'default-text-color' ) ) :
-	?>
-		.site-title a {
-			color: #<?php echo esc_attr( $text_color ); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
-endif; // dswoddil_header_style
-
-
-if ( ! function_exists( 'dswoddil_admin_header_style' ) ) :
-/**
- * Style the header image displayed on the Appearance > Header screen.
- *
- * @see dswoddil_custom_header_setup()
- *
- * @since DSW oddil 1.0
- */
-function dswoddil_admin_header_style() {
-?>
-	<style type="text/css" id="dswoddil-admin-header-css">
-	.appearance_page_custom-header #headimg {
-		background-color: #000;
-		border: none;
-		max-width: 1260px;
-		min-height: 48px;
-	}
-	#headimg h1 {
-		font-family: TheMixCE, sans-serif;
-		font-size: 18px;
-		line-height: 48px;
-		margin: 0 0 0 30px;
-	}
-	#headimg h1 a {
-		color: #fff;
-		text-decoration: none;
-	}
-	#headimg img {
-		vertical-align: middle;
-	}
-	</style>
-<?php
-}
-endif; // dswoddil_admin_header_style
-
-if ( ! function_exists( 'dswoddil_admin_header_image' ) ) :
-/**
- * Create the custom header image markup displayed on the Appearance > Header screen.
- *
- * @see dswoddil_custom_header_setup()
- *
- * @since DSW oddil 1.0
- */
-function dswoddil_admin_header_image() {
-?>
-	<div id="headimg">
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="">
+		// If we get this far, we have custom styles.
+		?>
+		<style type="text/css" id="dswoddil-header-css">
+		<?php
+			// Has the text been hidden?
+			if ( ! display_header_text() ) :
+		?>
+			.site-title,
+			.site-description {
+				clip: rect(1px 1px 1px 1px); /* IE7 */
+				clip: rect(1px, 1px, 1px, 1px);
+				position: absolute;
+			}
+		<?php
+			// If the user has set a custom color for the text, use that.
+			elseif ( $text_color != get_theme_support( 'custom-header', 'default-text-color' ) ) :
+		?>
+			.site-title a {
+				color: #<?php echo esc_attr( $text_color ); ?>;
+			}
 		<?php endif; ?>
-		<h1 class="displaying-header-text"><a id="name"<?php echo sprintf( ' style="color:#%s;"', get_header_textcolor() ); ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-	</div>
-<?php
-}
-endif; // dswoddil_admin_header_image
+		</style>
+		<?php
+	}
+} // dswoddil_header_style
+
+
+if ( ! function_exists( 'dswoddil_admin_header_style' ) ) {
+	/**
+	 * Style the header image displayed on the Appearance > Header screen.
+	 *
+	 * @see dswoddil_custom_header_setup()
+	 *
+	 * @since DSW oddil 1.0
+	 */
+	function dswoddil_admin_header_style() {
+	?>
+		<style type="text/css" id="dswoddil-admin-header-css">
+		.appearance_page_custom-header #headimg {
+			background-color: #000;
+			border: none;
+			max-width: 1260px;
+			min-height: 48px;
+		}
+		#headimg h1 {
+			font-family: TheMixCE, sans-serif;
+			font-size: 18px;
+			line-height: 48px;
+			margin: 0 0 0 30px;
+		}
+		#headimg h1 a {
+			color: #fff;
+			text-decoration: none;
+		}
+		#headimg img {
+			vertical-align: middle;
+		}
+		</style>
+	<?php
+	}
+} // dswoddil_admin_header_style
+
+if ( ! function_exists( 'dswoddil_admin_header_image' ) ) {
+	/**
+	 * Create the custom header image markup displayed on the Appearance > Header screen.
+	 *
+	 * @see dswoddil_custom_header_setup()
+	 *
+	 * @since DSW oddil 1.0
+	 */
+	function dswoddil_admin_header_image() {
+	?>
+		<div id="headimg">
+			<?php if ( get_header_image() ) : ?>
+			<img src="<?php header_image(); ?>" alt="">
+			<?php endif; ?>
+			<h1 class="displaying-header-text"><a id="name"<?php echo sprintf( ' style="color:#%s;"', get_header_textcolor() ); ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		</div>
+	<?php
+	}
+} // dswoddil_admin_header_image
