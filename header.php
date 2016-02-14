07e7f2b137cbe0bcadd630193e356b1ca1277c70
literat <tomas@litera.me>
2016-02-14 10:10:43 +0100

    Refactor header metas (refs #71)
      * add no-js class
      * set charset as variable
      * add profile
      * add pingback

--- header.php
+++ header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="no-js">
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php endif; ?>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
