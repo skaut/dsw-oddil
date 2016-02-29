be7dbd0031ab3b827fa912916dc36a41c06b7a92
literat <tomas@litera.me>
2016-02-29 20:49:57 +0100

    fix (HTTPS): Fixing connect to Facebook, set to https (closes #74)

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 				var js, fjs = d.getElementsByTagName(s)[0];
 				if (d.getElementById(id)) return;
 				js = d.createElement(s); js.id = id;
-				js.src = "http://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&appId=230812143598728&version=v2.0";
+				js.src = "https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&appId=230812143598728&version=v2.0";
 				fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		</script>
