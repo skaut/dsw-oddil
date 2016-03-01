36da6cb408cd2b6deec4929a0b778ea226bd3cb2
literat <tomas@litera.me>
2016-03-01 20:36:37 +0100

    Remove entire Facebook connect (refs #74)
      * this can be added by plugin or widget

--- footer.php
+++ footer.php
@@ -18,17 +18,6 @@
 			</div>
 		</div>
 
-		<div id="fb-root"></div>
-		<script>
-			(function(d, s, id) {
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s); js.id = id;
-				js.src = "https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&appId=230812143598728&version=v2.0";
-				fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-		</script>
-
 		<?php wp_footer(); ?>
 	</body>
 </html>
